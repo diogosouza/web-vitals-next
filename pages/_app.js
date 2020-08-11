5e1eb6b94b820029de637ced0a482c7f2d2ce621
Diogo Souza <diogosouzac@gmail.com>
2020-08-11 11:57:23 -0300

    teste

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,12 @@ function MyApp({ Component, pageProps }) {
 export function reportWebVitals(metric) {
   metric.label === "web-vital" && console.log(metric);
 
-  const body = JSON.stringify(metric);
-  const url = "/__appsignal-web-vitals";
+  // const body = JSON.stringify(metric);
+  // const url = "/__appsignal-web-vitals";
 
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  (navigator.sendBeacon && navigator.sendBeacon(url, body)) ||
-    fetch(url, { body, method: "POST", keepalive: true });
+  // // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  // (navigator.sendBeacon && navigator.sendBeacon(url, body)) ||
+  //   fetch(url, { body, method: "POST", keepalive: true });
 }
 
 export default MyApp;
